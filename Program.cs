24fab6faa222f5c996a5cfbd694a23cfad982db7
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-02 00:44:14 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 00000000000000000000000000001011;
+            uint res = 0;
+            int bits = 32;
+
+            while (n != 0)
+            {
+                res += (n & 1) << bits;
+                bits--;
+                n >>= 1;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
